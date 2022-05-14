5bac1f9b32b8d767102b7ecde80a3bea247d3839
Shebnem Bayezit <sebnembayezit2022@gmail.com>
2022-05-13 22:40:28 -0400

    print alphabet

---
+++ src/Eng_For_while_6_printAlphabet.java
@@ -0,0 +1,12 @@
+public class Eng_For_while_6_printAlphabet {
+    /*
+    Print Alphabet on the console.
+    OUTPUT : a b c .. .. .. .. y z
+     */
+    public static void main(String[] args) {
+        char i;
+        for ( i = 'a'; i <= 'z'; i++) {
+            System.out.print(i+ " ");
+        }
+    }
+}
